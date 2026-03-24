de2f8dc7cbb73131f9f3243610c66505d7e49260
redel-byte <alhabibridouane@gmail.com>
2026-03-24 15:19:14 +0100

    feat:completing the api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransferController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\SwaggerController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,11 +13,38 @@ Route::get('/docs/ui', [SwaggerController::class, 'ui']);
 Route::get('/docs/json', [SwaggerController::class, 'generate']);
 
 // Authentication Routes
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/login',[AuthController::class,'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 // Protected Routes
 Route::middleware('auth:api')->group(function () {
+    // User Profile
     Route::get('/me', [AuthController::class, 'me']);
+    Route::put('/me', [AuthController::class, 'update']);
+    Route::patch('/me/password', [AuthController::class, 'updatePassword']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Account Management
+    Route::get('/accounts', [AccountController::class, 'index']);
+    Route::post('/accounts', [AccountController::class, 'store']);
+    Route::get('/accounts/{id}', [AccountController::class, 'show']);
+    Route::post('/accounts/{id}/co-owners', [AccountController::class, 'addCoOwner']);
+    Route::delete('/accounts/{id}/co-owners/{userId}', [AccountController::class, 'removeCoOwner']);
+    Route::post('/accounts/{id}/guardian', [AccountController::class, 'assignGuardian']);
+    Route::patch('/accounts/{id}/convert', [AccountController::class, 'convert']);
+    Route::delete('/accounts/{id}', [AccountController::class, 'destroy']);
+
+    // Transfer Management
+    Route::post('/transfers', [TransferController::class, 'store']);
+    Route::get('/transfers/{id}', [TransferController::class, 'show']);
+    Route::get('/accounts/{id}/transactions', [TransferController::class, 'accountTransactions']);
+    Route::get('/transactions/{id}', [TransferController::class, 'transactionDetails']);
+});
+
+// Admin Routes
+Route::middleware(['auth:api', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/accounts', [AdminController::class, 'getAllAccounts']);
+    Route::patch('/accounts/{id}/block', [AdminController::class, 'blockAccount']);
+    Route::patch('/accounts/{id}/unblock', [AdminController::class, 'unblockAccount']);
+    Route::patch('/accounts/{id}/close', [AdminController::class, 'closeAccount']);
 });
